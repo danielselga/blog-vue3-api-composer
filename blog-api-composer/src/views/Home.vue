f26922399d7ba74be04f2ca974c2d6a8bfa27b9f
Daniel <danielselga@gmail.com>
2021-07-26 21:15:29 -0300

    props communication using v-for, props and the compostition API

--- blog-api-composer/src/views/Home.vue
+++ blog-api-composer/src/views/Home.vue
@@ -1,44 +1,26 @@
 <template>
   <div class="home">
-      <!-- <p>{{names}}</p> -->
-      <input type="text" v-model="search">
-      <p>search terms - {{search}}</p>
-      <button @click="handleClick">Stop watch</button>
-      <div v-for="name in matchingNames" :key="name">{{name}}</div>
-      <!-- O key deve ser indexado a um atributo que não se repita, uma utilização errada é um conjunto de string que se repete, uma correta é indexar a um id -->
+      <h1>home</h1>
+      <PostList :posts="posts"/>
   </div>
 </template>
 
 <script>
-import { computed, ref, watch, watchEffect } from 'vue'
-//quando usar o computed na composition API não esquecer de importar.
+import PostList from '../components/PostList.vue'
+import {ref} from 'vue'
 
 export default {
+    name: 'home',
+    components: {
+        PostList
+    },
     setup(){
-        const search = ref('')
-        const names = ref(['mario', 'luigi','peaches', 'Daniel', 'Georgia', 'koopa', 'zack'])
+        const posts = ref([
+            {title: 'Welcome to the blog', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio expedita illum, cumque deleniti quam aut iste laudantium magnam dolores. Nihil velit consequatur animi sit voluptate aperiam quis, perferendis provident molestias voluptatum accusantium nostrum quos, nesciunt voluptatibus explicabo pariatur. Dolorem obcaecati sint non, voluptatem cumque incidunt necessitatibus accusamus doloribus earum dolor architecto odit. Dolor consequuntur nemo saepe fuga neque at culpa esse earum numquam delectus sint expedita fugit, maxime, totam repellat nostrum porro odio quos provident inventore temporibus! Optio, unde est ducimus repellendus ipsum explicabo dolores ullam veritatis expedita ipsa laborum illum aut sed? Fuga aperiam assumenda rerum, aliquid placeat doloribus, reiciendis quia quibusdam magni nesciunt qui! Est nesciunt facilis enim. Harum similique incidunt inventore veritatis, corporis at temporibus voluptas molestiae provident cum doloremque accusamus debitis voluptatum totam recusandae quis quod, ullam libero delectus exercitationem. Expedita tempora similique dicta nulla eius, tenetur eligendi cumque ducimus, iste cupiditate fugit earum, quis facilis repellendus aspernatur perspiciatis sequi. Asperiores cupiditate sequi omnis consequatur eveniet, at earum consectetur aut amet sed eaque reprehenderit nobis facilis illum iusto, facere id quis. Fugiat ipsa maiores deserunt molestiae velit ut recusandae dolorem qui molestias, fugit assumenda corporis, non perferendis sunt quae perspiciatis? Repudiandae exercitationem quis minima sit architecto.', id: 1},
+            {title: 'top 5 CSS Tips', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio expedita illum, cumque deleniti quam aut iste laudantium magnam dolores. Nihil velit consequatur animi sit voluptate aperiam quis, perferendis provident molestias voluptatum accusantium nostrum quos, nesciunt voluptatibus explicabo pariatur. Dolorem obcaecati sint non, voluptatem cumque incidunt necessitatibus accusamus doloribus earum dolor architecto odit. Dolor consequuntur nemo saepe fuga neque at culpa esse earum numquam delectus sint expedita fugit, maxime, totam repellat nostrum porro odio quos provident inventore temporibus! Optio, unde est ducimus repellendus ipsum explicabo dolores ullam veritatis expedita ipsa laborum illum aut sed? Fuga aperiam assumenda rerum, aliquid placeat doloribus, reiciendis quia quibusdam magni nesciunt qui! Est nesciunt facilis enim. Harum similique incidunt inventore veritatis, corporis at temporibus voluptas molestiae provident cum doloremque accusamus debitis voluptatum totam recusandae quis quod, ullam libero delectus exercitationem. Expedita tempora similique dicta nulla eius, tenetur eligendi cumque ducimus, iste cupiditate fugit earum, quis facilis repellendus aspernatur perspiciatis sequi. Asperiores cupiditate sequi omnis consequatur eveniet, at earum consectetur aut amet sed eaque reprehenderit nobis facilis illum iusto, facere id quis. Fugiat ipsa maiores deserunt molestiae velit ut recusandae dolorem qui molestias, fugit assumenda corporis, non perferendis sunt quae perspiciatis? Repudiandae exercitationem quis minima sit architecto.', id: 2}
+            ])
 
-        const matchingNames = computed(() => {
-            return names.value.filter((name) => name.includes(search.value))
-        })
-
-        const stopWatch = watch(search, () => {
-          console.log('watch function ran')
-        })
-        //watch é um observable usado para monitorar o que está acontecendo no codigo.
-
-        const stopEffect = watchEffect(() => {
-            console.log('watchEfect ran', search.value)
-        })
-        //essa função diferentemente do watch dispara quando o setup inicia, se passarmos o valor de variavel que queremos observar ela vai disparar sempre
-        //que esse valor se alterar.
-
-        const handleClick = () => {
-            stopWatch()
-            stopEffect()
-        }
-
-        return {names, search, matchingNames, handleClick}
+            return {posts}
     }
 }
 </script>
